7e155b8b236b803fab1aba250cbeebc742319bce
Dok8tavo <vladicxo.pasxo@protonmail.ch>
2024-12-26 01:05:03 +0100

    added `Result`

---
+++ src/root.zig
@@ -0,0 +1,60 @@
+// MIT License
+//
+// Copyright (c) 2024 Dok8tavo
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+pub fn Result(comptime P: type, comptime F: type) type {
+    return union(enum) {
+        pass: Pass,
+        fail: Fail,
+
+        const Self = @This();
+
+        pub const Pass = P;
+        pub const Fail = F;
+
+        pub const Reversed = Result(Fail, Pass);
+
+        pub fn reverse(result: Self) Reversed {
+            return switch (result) {
+                .pass => |pass| Reversed{ .fail = pass },
+                .fail => |fail| Reversed{ .pass = fail },
+            };
+        }
+
+        pub fn nab(result: Self, capture: *Fail) ?Pass {
+            return switch (result) {
+                .pass => |pass| pass,
+                .fail => |fail| {
+                    capture.* = fail;
+                    return null;
+                },
+            };
+        }
+
+        pub fn get(result: Self) ?Pass {
+            return switch (result) {
+                .pass => |pass| pass,
+                .fail => null,
+            };
+        }
+    };
+}
